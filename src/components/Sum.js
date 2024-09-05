39510849f5fe78c4a05a13f2598def5171bd3750
md5800 <mohuya868@gmail.com>
2024-09-06 00:58:13 +0530

    test case with error

---
+++ src/components/Sum.js
@@ -0,0 +1,6 @@
+// export const sum = (a,b) => {
+//     return a + b;
+// };
+
+const sum = (a, b) => a + b;
+export default { sum };
---
+++ src/components/__tests__/sum.test.js
@@ -0,0 +1,7 @@
+import {sum} from "./sum";
+// const {sum} = require('./sum');
+
+test("sum should calculate sum of two numbers", () => {
+  const result = sum(3,4);
+  expect(result).toBe(7);
+});
